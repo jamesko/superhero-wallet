d0630e730e432b75c79c038c37ac46cb82b2ddb3
Nikita Cedrik <nikita.cedrik@gmail.com>
2021-02-18 13:04:34 +1000

    feat(temp): changes to TokensPreview

--- src/popup/router/pages/FungibleTokens/TokensPreview.vue
+++ src/popup/router/pages/FungibleTokens/TokensPreview.vue
@@ -1,65 +1,106 @@
 <template>
   <div class="tokens-preview">
     <div class="menu">
-      <a class="menu-item" :class="{ active: activeItem === 'tokens' }">
-        <TokenPiles />
-        {{ $t('pages.fungible-tokens.tokens') }}
-      </a>
+      <div class="tabs">
+        <button @click="activeTab = 'balances'" :class="{ active: activeTab === 'balances' }">
+          <Balances />
+          <span>{{ $t('pages.fungible-tokens.balances') }}</span>
+        </button>
+        <button @click="activeTab = 'all'" :class="{ active: activeTab === 'all' }">
+          <Balances />
+          <span>{{ $t('pages.fungible-tokens.all') }}</span>
+        </button>
+        <button @click="activeTab = 'favourites'" :class="{ active: activeTab === 'favourites' }">
+          <AddToFavourites />
+          <span>{{ $t('pages.fungible-tokens.favourites') }}</span>
+        </button>
+        <button v-show="!search" @click="search = true">
+          <Search />
+        </button>
+      </div>
+      <div v-show="!search && activeTab !== 'balances'" class="filters">
+        <button @click="sortingBy = ''" :class="{ active: sortingBy === '' }">
+          <span>{{ $t('pages.fungible-tokens.name') }}</span>
+          <Sort />
+        </button>
+        <button @click="sortingBy = 'price'" :class="{ active: sortingBy === 'price' }">
+          <span>{{ $t('pages.fungible-tokens.price') }}</span>
+          <Sort class="rotated" />
+        </button>
+        <button>
+          <span>{{ $t('pages.fungible-tokens.recent') }}</span>
+          <Filters />
+        </button>
+        <button>
+          <span>{{ $t('pages.fungible-tokens.trending') }}</span>
+          <Filters />
+        </button>
+      </div>
+      <div v-show="search" class="search">
+        <input
+          v-model="searchTerm"
+          :placeholder="$t('pages.fungible-tokens.searchPlaceholder')"
+          type="text"
+        />
+        <button @click="clearAndClose">
+          <Close />
+        </button>
+      </div>
+      <div v-show="activeTab === 'balances' && !search" class="account-balance">
+        <Avatar :address="account.address" />
+        <span>{{ `≈${formatCurrency(balanceCurrency)}` }}</span>
+      </div>
     </div>
-    <div class="search">
-      <input
-        v-model="searchTerm"
-        :placeholder="$t('pages.fungible-tokens.searchPlaceholder')"
-        type="text"
-      />
-      <Eraser v-if="searchTerm.length > 0" class="eracer-icon" @click="searchTerm = ''" />
-      <Search v-else class="search-icon" />
-    </div>
-    <TabsMenu v-model="activeTab" :tabOptions="tabs" />
-    <TokensList is-favourites :list="tokenList" :searchTerm="searchTerm" />
+    <TokensList
+      :is-favourites="activeTab !== 'balances'"
+      :list="tokenList"
+      :searchTerm="searchTerm"
+    />
   </div>
 </template>
 
 <script>
 import { pick } from 'lodash-es';
-import { mapState } from 'vuex';
-import TabsMenu from '../../components/TabsMenu';
+import { mapState, mapGetters } from 'vuex';
 import TokensList from '../../components/FungibleTokens/TokensList';
-import TokenPiles from '../../../../icons/token-piles.svg?vue-component';
+import Avatar from '../../components/Avatar';
+import Balances from '../../../../icons/balances.svg?vue-component';
+import AddToFavourites from '../../../../icons/add-to-favourites.svg?vue-component';
+import Sort from '../../../../icons/sort.svg?vue-component';
+import Filters from '../../../../icons/filters.svg?vue-component';
 import Search from '../../../../icons/search.svg?vue-component';
-import Eraser from '../../../../icons/eraser.svg?vue-component';
+import Close from '../../../../icons/close.svg?vue-component';
 
 export default {
-  name: 'fungible-tokens',
   components: {
-    TokenPiles,
+    Balances,
+    AddToFavourites,
+    Sort,
+    Filters,
     Search,
-    Eraser,
-    TabsMenu,
+    Close,
     TokensList,
+    Avatar,
   },
   data() {
     return {
-      activeItem: 'tokens',
+      search: false,
       searchTerm: '',
       activeTab: 'all',
-      tabs: [
-        {
-          name: 'all',
-          text: this.$t('pages.fungible-tokens.all'),
-        },
-        {
-          name: 'my-tokens',
-          text: this.$t('pages.fungible-tokens.myTokens'),
-        },
-      ],
+      sortingBy: '',
     };
   },
   subscriptions() {
     return pick(this.$store.state.observables, ['balance', 'balanceCurrency']);
   },
   computed: {
-    ...mapState('fungibleTokens', ['tokenBalances', 'availableTokens', 'aePublicData']),
+    ...mapState('fungibleTokens', [
+      'tokenBalances',
+      'availableTokens',
+      'aePublicData',
+      'favouriteTokens',
+    ]),
+    ...mapGetters(['account']),
 
     /**
      * Returns the default aeternity meta information
@@ -92,82 +133,226 @@ export default {
     tokenList() {
       return [
         ...this.aeternityToken,
-        ...(this.activeTab === this.tabs[1].name ? this.tokenBalances : this.tokenInfo),
-      ];
+        ...(this.activeTab === 'balances' ? this.tokenBalances : this.tokenInfo),
+      ]
+        .sort((a, b) => {
+          switch (this.sortingBy) {
+            case 'price':
+              return a.current_price > b.current_price;
+            default:
+              return 1;
+          }
+        })
+        .filter((t) =>
+          this.activeTab === 'favourites' ? this.favouriteTokens.includes(t.contract) : true,
+        );
     },
   },
+  methods: {
+    clearAndClose() {
+      this.searchTerm = '';
+      this.search = false;
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
-@import '../../../../styles/variables';
+@import '../../../../styles/typography';
 
-.menu {
-  margin-top: 3px;
-  padding: 7px;
-  background-color: $black-2;
-}
+.tokens-preview {
+  .menu {
+    position: fixed;
+    width: 100%;
+    background-color: $color-bg-3;
 
-.menu-item {
-  color: $gray-2;
-  padding: 10px 18px;
-  font-size: 15px;
-  font-weight: 500;
-  display: inline-block;
-  border-radius: 20px;
-  text-decoration: none;
+    .tabs,
+    .filters {
+      display: flex;
+      padding: 8px;
+      color: $color-light-grey;
+      border: 1px solid $color-black;
+      border-radius: 0 0 4px 4px;
 
-  svg {
-    margin-right: 5px;
-    vertical-align: sub;
-  }
-
-  &.active {
-    color: $accent-color;
-    background-color: $black-3;
-  }
-}
-
-.search {
-  display: flex;
-  align-items: center;
-  border: 1px solid transparent;
-
-  &:focus-within {
-    border-color: $secondary-color;
-  }
-
-  input {
-    font-size: 14px;
-    color: $white-1;
-    height: 40px;
-    padding: 0 15px;
-    flex-grow: 1;
-
-    &::placeholder {
-      color: $gray-2;
-    }
-
-    &:focus {
-      outline: none;
-      border: none;
-
-      ~ .search-icon {
-        display: none;
+      > button {
+        display: flex;
+        align-items: center;
       }
     }
 
-    ~ * {
-      margin-right: 15px;
+    .tabs {
+      > button {
+        border-radius: 20px;
+        padding: 8px;
+        margin-right: 8px;
+
+        @extend %face-sans-16-medium;
+
+        &:last-of-type {
+          margin-right: 0;
+          margin-left: auto;
+
+          svg:hover {
+            color: $color-green;
+            opacity: 1;
+            cursor: pointer;
+          }
+        }
+
+        &:hover:not(:last-of-type):not(.active) {
+          background-color: $color-hover;
+          color: $color-white;
+          transition: all 0.12s ease-in-out;
+          cursor: pointer;
+
+          svg {
+            opacity: 1;
+          }
+        }
+
+        &.active {
+          color: $color-green;
+
+          svg {
+            color: inherit;
+            opacity: 1;
+          }
+        }
+
+        span {
+          margin-left: 4px;
+        }
+
+        svg {
+          color: $color-white;
+          opacity: 0.7;
+          width: 24px;
+          height: 24px;
+        }
+      }
+    }
+
+    .filters {
+      padding-left: 16px;
+      padding-right: 14px;
+
+      button {
+        @extend %face-sans-15-medium;
+
+        padding: 0;
+        line-height: 24px;
+        margin-right: 24px;
+
+        &:last-of-type {
+          margin-right: 0;
+        }
+
+        &:hover {
+          color: $color-light-grey;
+          transition: all 0.12s ease-in-out;
+          cursor: pointer;
+
+          svg {
+            color: $color-green;
+          }
+        }
+
+        &.active {
+          color: $color-green;
+        }
+
+        span {
+          margin-right: 2px;
+        }
+
+        svg {
+          color: $color-white;
+          width: 16px;
+          height: 16px;
+
+          &.rotated {
+            transform: rotate(180deg);
+          }
+        }
+      }
+    }
+
+    .search {
+      display: flex;
+      align-items: center;
+      border-radius: 6px;
+      border-top: 1px solid transparent;
+      border-bottom: 1px solid transparent;
+
+      &:focus-within {
+        border-color: $color-blue;
+      }
+
+      input {
+        font-size: 14px;
+        color: $white-1;
+        height: 40px;
+        padding: 0 15px;
+        flex-grow: 1;
+
+        &::placeholder {
+          color: $gray-2;
+        }
+
+        &:focus {
+          outline: none;
+          border: none;
+        }
+
+        ~ * {
+          margin-right: 15px;
+        }
+      }
+
+      button {
+        width: 30px;
+        height: 30px;
+        align-items: center;
+        display: flex;
+        border-radius: 16px;
+
+        &:hover {
+          background-color: $color-hover;
+          cursor: pointer;
+
+          svg {
+            opacity: 1;
+          }
+        }
+
+        svg {
+          color: $color-white;
+          width: 18px;
+          height: 18px;
+          opacity: 0.44;
+        }
+      }
+    }
+
+    .account-balance {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding: 8px 16px;
+      background: rgba(17, 97, 254, 0.15);
+      border-radius: 4px;
+
+      @extend %face-sans-20-regular;
+
+      .avatar {
+        width: 32px;
+        height: 32px;
+      }
     }
   }
 
-  .search-icon {
-    color: $accent-color;
-  }
-
-  .eracer-icon :hover {
-    cursor: pointer;
+  .token-list {
+    padding-top: 100px;
   }
 }
 </style>
