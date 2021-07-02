7b31558457451db418010d722c08fc643b8f2a29
etharner <etharner@gmail.com>
2021-07-02 20:48:36 +1000

    chore(amount-input): use TokenAmount for right side

--- src/popup/router/components/AmountInput.vue
+++ src/popup/router/components/AmountInput.vue
@@ -4,37 +4,31 @@
     type="number"
     v-bind="$attrs"
     :error="$attrs.error || !!$attrs.value && $attrs.value <= 0"
-    :error-message="$attrs.errorMessage || $t('pages.tipPage.insufficientAmountError')"
+    :error-message="$attrs['error-message'] || $t('pages.tipPage.insufficientAmountError')"
     placeholder="0.00"
     :label="$attrs.label || $t('pages.tipPage.amountLabel')"
     @input="$emit('input', $event)"
   >
-    <template slot="right">
-      <span class="token">{{ selectedToken ? selectedToken.symbol : 'AE' }}</span>
-      <span
-        class="amount"
-        data-cy="amount-currency"
-      >
-        {{ `(${formatCurrency(selectedToken ? 0 : currencyAmount)})` }}
-      </span>
-    </template>
+    <TokenAmount
+      slot="right"
+      :amount="+$attrs.value || 0"
+      :symbol="selectedToken ? selectedToken.symbol : 'AE'"
+    />
   </InputField>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapState } from 'vuex';
 import InputField from './InputField';
+import TokenAmount from './TokenAmount';
 
 export default {
   components: {
     InputField,
+    TokenAmount,
   },
   computed: {
-    ...mapGetters(['formatCurrency']),
-    ...mapGetters('fungibleTokens', ['selectedToken']),
-    currencyAmount() {
-      return ((this.$attrs.value || 0) * this.$store.getters.currentCurrencyRate).toFixed(2);
-    },
+    ...mapState('fungibleTokens', ['selectedToken']),
   },
 };
 </script>
@@ -45,15 +39,5 @@ export default {
 
 .amount-input {
   white-space: nowrap;
-
-  .token {
-    margin-right: 2px;
-    font-weight: 500;
-    color: variables.$color-blue;
-  }
-
-  .amount {
-    color: variables.$color-dark-grey;
-  }
 }
 </style>
